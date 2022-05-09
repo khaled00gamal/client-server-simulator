9a69d449a086da0b16bf2b8e70409422914aeff6
Khaled gamal <khaled2000s.kg@gmail.com>
2022-05-10 01:17:38 +0200

    parse file & start connection

--- multithreadserver.py
+++ multithreadserver.py
@@ -1,4 +1,4 @@
-from socket import *
+import socket 
 import threading
 
 class socketThread(threading.Thread):
@@ -9,27 +9,27 @@ class socketThread(threading.Thread):
         
 
     def run(self):
-        try:
+        
             request = self.connSocket.recv(1024).decode()
             print(request)
 
-            headers = request.split('\n')
-            method = headers[0].split()[0]
-            print(method)
-            filename = headers[0].split()[1]
-            requestedFile = open(filename[1:])
-            protocol = headers[0].split()[2]
-            if method == "GET":
-                data = requestedFile.read()
-                requestedFile.close()
-                response = 'HTTP/1.0 200 OK \n\n'  + data
-                self.connSocket.sendall(response.encode())
-            elif method == "POST":
-                print(method)
+        #     headers = request.split('\n')
+        #     method = headers[0].split()[0]
+        #     print(method)
+        #     filename = headers[0].split()[1]
+        #     protocol = headers[0].split()[2]
+        #     if method == "GET":
+        #         requestedFile = open(filename[1:])
+        #         data = requestedFile.read()
+        #         requestedFile.close()
+        #         response = 'HTTP/1.0 200 OK \n\n'  + data
+        #         self.connSocket.sendall(response.encode())
+        #     elif method == "POST":
+        #         print(method)
 
-        except IOError:
-            response= 'HTTP/1.0 404 Not Found\n\n404 File Not Found'
-            self.connSocket.sendall(response.encode())
+        # except IOError:
+        #     response= 'HTTP/1.0 404 Not Found\n\n404 File Not Found'
+        #     self.connSocket.sendall(response.encode())
 
 
 
@@ -38,8 +38,8 @@ class socketThread(threading.Thread):
 
 
 serverPort = 1200
-serverHost = "127.0.0.1"
-serverSocket = socket(AF_INET, SOCK_STREAM)
+serverHost = socket.gethostbyname(socket.gethostname())
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.bind((serverHost, serverPort))
 threads = []
 
@@ -48,5 +48,4 @@ while True:
     conn, addr = serverSocket.accept()
     tcpthreads = socketThread(conn, addr)
     tcpthreads.start()
-    threads.append(tcpthreads)
     conn.close()
