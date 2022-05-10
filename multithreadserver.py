710335ce22b21050fa596f3dc288c456488469ff
Khaled gamal <khaled2000s.kg@gmail.com>
2022-05-10 15:30:58 +0200

    client trial
    first try to handle client GET and POST

--- multithreadserver.py
+++ multithreadserver.py
@@ -9,27 +9,27 @@ class socketThread(threading.Thread):
         
 
     def run(self):
-        
+        try:
             request = self.connSocket.recv(1024).decode()
             print(request)
 
-        #     headers = request.split('\n')
-        #     method = headers[0].split()[0]
-        #     print(method)
-        #     filename = headers[0].split()[1]
-        #     protocol = headers[0].split()[2]
-        #     if method == "GET":
-        #         requestedFile = open(filename[1:])
-        #         data = requestedFile.read()
-        #         requestedFile.close()
-        #         response = 'HTTP/1.0 200 OK \n\n'  + data
-        #         self.connSocket.sendall(response.encode())
-        #     elif method == "POST":
-        #         print(method)
+            headers = request.split('\n')
+            method = headers[0].split()[0]
+            print(method)
+            filename = headers[0].split()[1]
+            protocol = headers[0].split()[2]
+            if method == "GET":
+                requestedFile = open(filename[1:])
+                data = requestedFile.read()
+                requestedFile.close()
+                response = 'HTTP/1.0 200 OK \n\n'  + data
+                self.connSocket.sendall(response.encode())
+            elif method == "POST":
+                print(method)
 
-        # except IOError:
-        #     response= 'HTTP/1.0 404 Not Found\n\n404 File Not Found'
-        #     self.connSocket.sendall(response.encode())
+        except IOError:
+            response= 'HTTP/1.0 404 Not Found\n\n404 File Not Found'
+            self.connSocket.sendall(response.encode())
 
 
 
