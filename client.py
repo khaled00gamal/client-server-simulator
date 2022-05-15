a1aacb5bd97be8b9571d045c5fd212b93b0d8bbf
Areej Salahuddin <es-Areej.abdelsamie2023@alexu.edu.eg>
2022-05-15 18:22:58 +0200

    support png files

--- client.py
+++ client.py
@@ -12,21 +12,29 @@ cache = {}
 index = 1
 
 
-def compose_request(method, filename, hostname, port):
+def compose_request(method, filename, extension, hostname, port):
+    request_in_bytes = ""
     if method == 'GET':
-        request = "GET /%s HTTP/1.1\r\nHost:%s:%s\r\n\r\n" % (filename, hostname, port)
+        request= "GET /%s HTTP/1.1\r\nHost:%s:%s\r\n\r\n" % (filename, hostname, port)
+        request_in_bytes = request.encode()
     else:
         request = "POST /%s HTTP/1.1\r\nHost:%s:%s\r\n\r\n" % (filename, hostname, port)
         file_exists = exists(filename)
         if file_exists:
-            with open(filename) as file:
-                payload = file.read()
-                request += payload + "\r\n"
+            if extension == "txt" or extension == "html":
+                with open(filename) as file:
+                    payload = file.read()
+                    request += payload + "\r\n"
+                    request_in_bytes = request.encode()
+            elif extension == "png":
+                with open(filename, mode='rb') as file:
+                    img = file.read()
+                    request_in_bytes = request.encode() + img
         else:
             print("File you're trying to access does not exist!")
             return 0
 
-    return request
+    return request_in_bytes
 
 
 with open(filename) as f:
@@ -36,26 +44,34 @@ for operation in operations:
     method = operation.split()[0]
     filename = operation.split()[1][1:]
     hostname = operation.split()[2]
+    extension = filename.split(".")[1]
     no_of_words = len(operation.split(' '))
     if no_of_words < 4:
         port = 80
     else:
         port = int(operation.split()[3])
-    output_request = compose_request(method, filename, hostname, port)
 
-    if output_request == 0:
+    output_request = compose_request(method, filename, extension, hostname, port)
+
+    if output_request == 0:  # POST Request with a non-existent client file
         print("Invalid Request....Will not be connected to server!")
         continue
-
+    double_break_line = "\r\n\r\n"
+    headers_request = output_request.partition(double_break_line.encode())[0]
+    request_without_payload = headers_request.decode() + double_break_line
     cached = 0
     for key in cache:
-        if output_request in cache[key]['Request']:
+        if request_without_payload in cache[key]['Request']:
             cached = 1
             print("Found in Cache!")
             print(cache[key]['Response'])
             if method == "GET":
                 if cache[key]['Response'].split()[1] == "200":
-                    print("Requested File content: \n" + cache[key]['Content'])
+                    if extension == "txt" or extension == "html":
+                        print("Requested File content: \n" + cache[key]['Content'])
+                    elif extension == "png":
+                        image = Image.open("client" + filename)
+                        image.show()
 
     if cached:
         continue
@@ -65,25 +81,43 @@ for operation in operations:
         try:
             s.connect((hostname, port))
             print("Connected to server.")
-            request_in_bytes = output_request.encode()
-            s.sendall(request_in_bytes)
+            s.sendall(output_request)
             cache[index] = {}
-            cache[index]['Request'] = output_request
+            cache[index]['Request'] = request_without_payload
             print("Request is sent.")
-            response = s.recv(4096).decode()
-            cache[index]['Response'] = response
-            print(response)
+            response_in_bytes = s.recv(10000000)
+            headers_response = response_in_bytes.partition(double_break_line.encode())[0]
+            received_data = response_in_bytes.partition(double_break_line.encode())[2]
+            response_without_payload = headers_response.decode() + double_break_line
             if method == 'GET':
-                if response.split()[1] == "200":
-                    received_data = response.partition("\r\n\r\n")[2]
-                    fp = open("client" + filename, "w")
-                    fp.write(received_data)
-                    cache[index]['Content'] = received_data
-                    fp.close()
-
+                if response_without_payload.split()[1] == "200":
+                    if extension == "txt" or extension == "html":
+                        print(response_in_bytes.decode())
+                        cache[index]['Response'] = response_in_bytes.decode()
+                        received_data = received_data.decode()
+                        fp = open("client" + filename, "w")
+                        fp.write(received_data)
+                        cache[index]['Content'] = received_data
+                        fp.close()
+                    elif extension == "png":
+                        cache[index]['Response'] = response_without_payload
+                        print(response_without_payload)
+                        fp = open("client" + filename, mode='wb')
+                        fp.write(received_data)
+                        fp.close()
+                        print("PNG received. Displaying....")
+                        image = Image.open("client" + filename)
+                        image.show()
+            else:
+                if extension == "txt" or extension == "html":
+                    print(response_in_bytes.decode())
+                    cache[index]['Response'] = response_in_bytes.decode()
+                elif extension == "png":
+                    cache[index]['Response'] = response_without_payload
+                    print(response_without_payload)
         except ConnectionRefusedError:
             print(f"Failed to connect with server - {hostname}:{port}!")
-        sleep(2)
+
     index = index + 1
 
-
+print(cache)
--- server.py
+++ server.py
@@ -2,40 +2,72 @@ import socket
 from os.path import exists
 from _thread import *
 import threading
+from PIL import Image
 
 print_lock = threading.Lock()  # race condition
 
 
 def compose_response(request):
-    method = request.split()[0]
-    filename = request.split()[1][1:]
-    HTTP_version = request.split()[2][0:8]
+    print("\n\n")
+    print(request)
+    print("\n\n")
+    response_in_bytes = ""
+    double_break_line = "\r\n\r\n"
+    headers = request.partition(double_break_line.encode())[0]
+    received_data = request.partition(double_break_line.encode())[2]
+    method = headers.decode().split()[0]
+    filename = headers.decode().split()[1][1:]
+    extension = filename.split(".")[1]
 
     if method == 'GET':
+        print(headers.decode())
         file_exists = exists(filename)
         if file_exists:
-            with open(filename) as file:
-                data = file.read()
-            response = "%s 200 OK\r\n\r\n%s\r\n" % (HTTP_version, data)
+            if extension == "txt" or extension == "html":
+                with open(filename) as file:
+                    data = file.read()
+                response = "HTTP/1.1 200 OK\r\n\r\n%s\r\n" % data
+                response_in_bytes = response.encode()
+
+            elif extension == "png":
+                with open(filename, mode='rb') as file:
+                    img = file.read()
+
+                headers = "HTTP/1.1 200 OK\r\n\r\n"
+                response_in_bytes = headers.encode() + img
 
         else:
-            response = "%s 404 Not Found\r\n\r\n" % HTTP_version
+            response = "HTTP/1.1 404 Not Found\r\n\r\n"
+            response_in_bytes = response.encode()
 
     else:
-        received_data = request.partition("\r\n\r\n")[2]
-        fp = open("server" + filename, "w")
-        fp.write(received_data)
-        fp.close()
-        response = "%s 200 OK\r\n\r\n" % HTTP_version
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+        if extension == "txt" or extension == "html":
+            print(request.decode())
+            received_data = received_data.decode()
+            fp = open("server" + filename, "w")
+            fp.write(received_data)
+            fp.close()
 
-    return response
+        elif extension == "png":
+            print(headers.decode() + double_break_line)
+            fp = open("server" + filename, mode='wb')
+            fp.write(received_data)
+            fp.close()
+            print("PNG received. Displaying....")
+            image = Image.open("server" + filename)
+            image.show()
+
+        response_in_bytes = response.encode()
+
+    return response_in_bytes
 
 
 def client_thread(connection):
     with connection:
         while 1:
             try:
-                request = connection.recv(4096).decode()
+                request = connection.recv(1000000)
 
             except socket.timeout:
                 print("Connection is closed due to inactivity (TIMEOUT).")
@@ -47,10 +79,8 @@ def client_thread(connection):
                 print_lock.release()
                 break
 
-            print("REQUEST: " + request)
             response = compose_response(request)
-            response_in_bytes = response.encode()
-            connection.sendall(response_in_bytes)
+            connection.sendall(response)
 
 
 HOST = "127.0.0.1"
